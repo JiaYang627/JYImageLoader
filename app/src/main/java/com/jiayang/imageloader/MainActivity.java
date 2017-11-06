18f0a7e476d07c5478df4a9b1eebbe022bf031ef
JiaYang <JiaYang627@163.com>
2017-11-06 15:46:25 +0800

    优化

--- app/src/main/java/com/jiayang/imageloader/MainActivity.java
+++ app/src/main/java/com/jiayang/imageloader/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements AbsListView.OnScr
                 "http://pic47.nipic.com/20140830/7487939_180041822000_2.jpg",
                 "http://pic41.nipic.com/20140518/4135003_102912523000_2.jpg",
                 "http://img2.imgtn.bdimg.com/it/u=1133260524,1171054226&fm=21&gp=0.jpg",
-                "http://h.hiphotos.baidu.com/image/pic/item/3b87e950352ac65c0f1f6e9efff2b21192138ac0.jpg",
+                "http://p5.so.qhimgs1.com/bdr/_240_/t01a58ae9d09c0e4f4b.jpg",
                 "http://pic42.nipic.com/20140618/9448607_210533564001_2.jpg",
                 "http://pic10.nipic.com/20101027/3578782_201643041706_2.jpg",
                 "http://p0.so.qhimgs1.com/bdr/_240_/t017e66f7d6f53727dc.jpg",//
