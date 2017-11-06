fc6701b0d7cb310f3d133100a0408647663eaf30
JiaYang <JiaYang627@163.com>
2017-11-06 15:23:16 +0800

    Initial commit

---
+++ app/src/main/java/com/jiayang/imageloader/MainActivity.java
@@ -0,0 +1,191 @@
+package com.jiayang.imageloader;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.jiayang.imageloader.utils.MyUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AbsListView.OnScrollListener {
+
+
+    private List<String> mUrList = new ArrayList<String>();
+    private int mImageWidth;
+    private boolean mIsWifi;
+    private boolean mCanGetBitmapFromNetWork;
+    private GridView mImageGridView;
+    private boolean mIsGridViewIdle = true;
+    private ImageAdapter mImageAdapter;
+    private ImageLoader mImageLoader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+        mImageLoader = ImageLoader.build(MainActivity.this);
+
+    }
+
+    private void initData() {
+        String[] imageUrls = {
+                "http://b.hiphotos.baidu.com/zhidao/pic/item/a6efce1b9d16fdfafee0cfb5b68f8c5495ee7bd8.jpg",
+                "http://pic47.nipic.com/20140830/7487939_180041822000_2.jpg",
+                "http://pic41.nipic.com/20140518/4135003_102912523000_2.jpg",
+                "http://img2.imgtn.bdimg.com/it/u=1133260524,1171054226&fm=21&gp=0.jpg",
+                "http://h.hiphotos.baidu.com/image/pic/item/3b87e950352ac65c0f1f6e9efff2b21192138ac0.jpg",
+                "http://pic42.nipic.com/20140618/9448607_210533564001_2.jpg",
+                "http://pic10.nipic.com/20101027/3578782_201643041706_2.jpg",
+                "http://p0.so.qhimgs1.com/bdr/_240_/t017e66f7d6f53727dc.jpg",//
+                "http://img2.3lian.com/2014/c7/51/d/26.jpg",
+                "http://img3.3lian.com/2013/c1/34/d/93.jpg",
+                "http://b.zol-img.com.cn/desk/bizhi/image/3/960x600/1375841395686.jpg",
+                "http://p3.so.qhimgs1.com/bdr/_240_/t016b1d4fb05c618403.jpg",//
+                "https://ps.ssl.qhimg.com/sdmt/162_135_100/t01864c0352c454ae74.jpg",
+                "http://imgrt.pconline.com.cn/images/upload/upc/tx/wallpaper/1210/17/c1/spcgroup/14468225_1350443478079_1680x1050.jpg",
+                "http://pic41.nipic.com/20140518/4135003_102025858000_2.jpg",
+                "https://ps.ssl.qhimg.com/sdmt/187_135_100/t015f4f7fc523e92ef7.jpg",
+                "http://pic.58pic.com/58pic/13/00/22/32M58PICV6U.jpg",
+                "https://ps.ssl.qhimg.com/sdmt/186_135_100/t0136e2bfc8de5e8d6a.jpg",
+                "http://h.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=429e7b1b92ef76c6d087f32fa826d1cc/7acb0a46f21fbe09cc206a2e69600c338744ad8a.jpg",
+                "http://pica.nipic.com/2007-12-21/2007122115114908_2.jpg",
+                "http://p5.so.qhimgs1.com/bdr/_240_/t01e0b9f718f7212d4b.jpg",
+                "http://photo.loveyd.com/uploads/allimg/080618/1110324.jpg",
+                "http://img4.duitang.com/uploads/item/201404/17/20140417105820_GuEHe.thumb.700_0.jpeg",
+                "http://p2.so.qhimgs1.com/bdr/_240_/t0107fe65aff9c38f4e.jpg",
+                "http://img4.duitang.com/uploads/item/201404/17/20140417105856_LTayu.thumb.700_0.jpeg",
+                "http://img04.tooopen.com/images/20130723/tooopen_20530699.jpg",
+                "http://p1.so.qhimgs1.com/bdr/_240_/t01388378fd7ca0d631.jpg",
+                "http://pic.dbw.cn/0/01/33/59/1335968_847719.jpg",
+                "http://a.hiphotos.baidu.com/image/pic/item/a8773912b31bb051a862339c337adab44bede0c4.jpg",
+                "http://h.hiphotos.baidu.com/image/pic/item/f11f3a292df5e0feeea8a30f5e6034a85edf720f.jpg",
+                "http://img0.pconline.com.cn/pconline/bizi/desktop/1412/ER2.jpg",
+                "http://pic.58pic.com/58pic/11/25/04/91v58PIC6Xy.jpg",
+                "http://img3.3lian.com/2013/c2/32/d/101.jpg",
+                "http://pic25.nipic.com/20121210/7447430_172514301000_2.jpg",
+                "http://img02.tooopen.com/images/20140320/sy_57121781945.jpg",
+                "http://p5.so.qhimgs1.com/bdr/_240_/t019c1b8deda199519b.jpg"
+        };
+        for (String url : imageUrls) {
+            mUrList.add(url);
+        }
+        int screenWidth = MyUtils.getScreenMetrics(this).widthPixels;
+        int space = (int) MyUtils.dp2px(this, 20f);     // 分割距离
+        mImageWidth = (screenWidth - space) / 3;        // 宽度
+        mIsWifi = MyUtils.isWifi(this);
+        if (mIsWifi) {
+            mCanGetBitmapFromNetWork = true;
+        }
+
+
+
+    }
+
+    private void initView() {
+        mImageGridView = (GridView) findViewById(R.id.gridView);
+        mImageAdapter = new ImageAdapter(this);
+        mImageGridView.setAdapter(mImageAdapter);
+        mImageGridView.setOnScrollListener(this);
+
+        if (!mIsWifi) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("初次使用会从网络下载大概5MB的图片，确认要下载吗？");
+            builder.setTitle("注意");
+            builder.setPositiveButton("是", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    mCanGetBitmapFromNetWork = true;
+                    mImageAdapter.notifyDataSetChanged();
+                }
+            });
+            builder.setNegativeButton("否", null);
+            builder.show();
+        }
+    }
+
+    private class ImageAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
+        private Drawable mDefaultBitmapDrawable;
+
+        private ImageAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+            mDefaultBitmapDrawable = context.getResources().getDrawable(R.drawable.image_default);
+        }
+
+        @Override
+        public int getCount() {
+            return mUrList.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mUrList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.image_list_item, parent, false);
+                holder = new ViewHolder();
+                holder.imageView = (ImageView) convertView.findViewById(R.id.image);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            ImageView imageView = holder.imageView;
+            final String tag = (String) imageView.getTag();
+            final String uri = getItem(position);
+            if (!uri.equals(tag)) {
+                imageView.setImageDrawable(mDefaultBitmapDrawable);
+            }
+            if (mIsGridViewIdle && mCanGetBitmapFromNetWork) {
+                imageView.setTag(uri);
+                mImageLoader.bindBitmap(uri, imageView, mImageWidth, mImageWidth);
+            }
+            return convertView;
+        }
+
+    }
+
+    private static class ViewHolder {
+        public ImageView imageView;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+            mIsGridViewIdle = true;
+            mImageAdapter.notifyDataSetChanged();
+        } else {
+            mIsGridViewIdle = false;
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem,
+                         int visibleItemCount, int totalItemCount) {
+        // ignored
+
+    }
+}
---
+++ app/src/main/java/com/jiayang/imageloader/ui/SquareImageView.java
@@ -0,0 +1,28 @@
+package com.jiayang.imageloader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by 张 奎 on 2017-11-06 14:31.
+ */
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
